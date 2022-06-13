8f2ae636b7ba010cc2b78934bdcb2a264af7e2d4
Alexander Nuykin <scepticnav@gmail.com>
2022-06-13 17:00:23 +0300

    Add Task68

---
+++ Task68/Program.cs
@@ -0,0 +1,39 @@
+﻿/* 
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 0, n = 0 -> A(m,n) = 1
+m = 0, n = 1 -> A(m,n) = 2
+m = 3, n = 5 -> A(m,n) = 253
+m = 3, n = 8 -> A(m,n) = 2045
+*/
+
+int GetNumber(string msg)                                                   // Метод проверки ввода пользователем числа
+{
+    while (true)
+    {
+        Console.WriteLine(msg);
+        string valueFromConsole = Console.ReadLine();
+
+        if (int.TryParse(valueFromConsole, out int number))
+        {
+            return number;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число. Нужно ввести число.");
+        }
+    }
+}
+
+int AckermanFunction(int m, int n)                                          // Метод расчета функции Аккермана
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return AckermanFunction(m - 1, 1);
+    if (m > 0 && n > 0) return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+    return AckermanFunction(m, n);
+}
+
+int number1 = GetNumber("Введите первый аргумент функции Аккермана m");                                 // Код программы
+int number2 = GetNumber("Введите второй аргумент функции Аккермана n");
+int ResultAckermanFunction = AckermanFunction(number1,number2);
+Console.WriteLine($"Результат фунции Аккермана A({number1},{number2}) = {ResultAckermanFunction}");
